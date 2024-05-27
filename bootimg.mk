38991a8589d05d7d260d9956c3c27c1a19f39501
TheNoobDevs <thenoobdevs2024@gmail.com>
2024-05-27 07:53:47 +0100

    Update bootimg.mk

--- bootimg.mk
+++ bootimg.mk
@@ -19,12 +19,12 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_k
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	@echo "----- Lying about SEAndroid state to Samsung bootloader ------"
 	$(hide) echo -n "SEANDROIDENFORCE" >> $@
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 	$(hide) $(AVBTOOL) add_hash_footer \
 	  --image $@ \
 	  --partition_size $(BOARD_RECOVERYIMAGE_PARTITION_SIZE) \
 	  --partition_name recovery $(INTERNAL_AVB_RECOVERY_SIGNING_ARGS) \
 	  $(BOARD_AVB_RECOVERY_ADD_HASH_FOOTER_ARGS)
-    	$(hide) $(AVBTOOL) add_hash_footer --image $@ --partition_size $(BOARD_RECOVERYIMAGE_PARTITION_SIZE) --partition_name recovery --algorithm $(BOARD_AVB_RECOVERY_ALGORITHM) --key $(BOARD_AVB_RECOVERY_KEY_PATH)
 	@echo "Made recovery image: $@"
 	$(hide) tar -C $(PRODUCT_OUT) -c recovery.img > $(FLASH_IMAGE_TARGET)
 
